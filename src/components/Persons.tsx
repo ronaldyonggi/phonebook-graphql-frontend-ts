f26b6301aea2c3200678efdf79c4d5c811c089f9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-05 20:22:32 +0800

    add functionality to query address of a person on demand

--- src/components/Persons.tsx
+++ src/components/Persons.tsx
@@ -1,15 +1,52 @@
+import { gql, useQuery } from "@apollo/client"
+import { useState } from "react"
+import { Person } from "../types"
+import PersonItem from "./PersonItem"
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
 
 interface PersonsProps {
-  persons: Array<{name: string, phone: string}>
+  persons: Array<Person>
 }
 
 const Persons = ({ persons }: PersonsProps ) => {
+  const [nameToSearch, setNameToSearch] = useState<string | null>(null)
+  const result = useQuery(FIND_PERSON, {
+    variables: { nameToSearch },
+    skip: !nameToSearch
+  })
+
+  // If name search is provided and a matching name is found, execute the following:
+  if (nameToSearch && result.data) {
+    return (
+      <PersonItem 
+        person={result.data.findPerson}
+        onClose = {() => setNameToSearch(null)}
+      />
+    )
+  }
+
   return (
     <div>
       <h2>Persons</h2>
       {persons.map(p => 
         <div key={p.name}>
           {p.name} {p.phone}
+          <button onClick={() => setNameToSearch(p.name)}>
+            show address
+          </button>
         </div>
         )}
     </div>
