e0e82724b285d1f995d3237a682f7dc02e7ff136
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-05 12:11:48 +0800

    set up apollo client starter code

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+const query = gql`
+  query {
+    allPersons {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query })
+  .then(res => console.log(res.data))
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
