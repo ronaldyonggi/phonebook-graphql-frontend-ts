4662f67838371aaa01831e1254cb424119b2d630
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-05 13:43:23 +0800

    initial app state using graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import { useState } from 'react'
+import { gql, useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
 
-function App() {
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <Persons persons={result.data.allPersons} />
+  )
 }
 
 export default App
