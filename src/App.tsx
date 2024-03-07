6b7c67181de113ededb215c913b05862892d57a5
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-07 09:12:46 +0800

    add error notification functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import { useQuery } from "@apollo/client";
-import Persons from "./components/Persons";
-import PersonForm from "./components/PersonForm";
-import { ALL_PERSONS } from "./graphql/person";
+import { useQuery } from '@apollo/client';
+import Persons from './components/Persons';
+import PersonForm from './components/PersonForm';
+import { ALL_PERSONS } from './graphql/person';
+import { useState } from 'react';
+import ErrorNotification from './components/ErrorNotification';
 
 const App = () => {
-  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const result = useQuery(ALL_PERSONS);
+
+  const notificationHelper = (message: string) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000);
+  }
 
   if (result.loading) {
-    return <div>loading...</div>
+    return <div>loading...</div>;
   }
 
   return (
     <div>
+      <ErrorNotification message={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notificationHelper} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
